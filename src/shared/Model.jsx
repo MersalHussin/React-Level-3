66eefd5ec4bb8eaef7d4a4369557a4a676bb177c
mersalhussin <mersalhussin@gmail.com>
2024-12-27 18:45:20 +0200

    Upload Files

--- src/shared/Model.jsx
+++ src/shared/Model.jsx
@@ -21,7 +21,57 @@ function Model( {closeFunc, children} ) {
   const [showModel, setshowModel] = useState(false);
 
   return (
-
+<>
+<Helmet>
+          <style type="text/css">{`
+          
+          .parent-of-model{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.45);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  }
+  
+  .model{
+    position: fixed;
+    background-color: whitesmoke;
+    width: 500px;
+    height: 500px;
+    border-radius: 5px;
+    /* display: none; */
+    scale: 1;
+    // transition: 0.5s;
+    /* transform: translateY(-100vh) rotate(360deg); */
+  }
+  
+  /* .forgot.show{
+    display: flex;
+    scale: 1;
+  } */
+  .model input{
+    box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.068);
+  }
+  
+  .model .fa-xmark{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    color: black;
+  }
+  .model .fa-xmark:hover{
+    transition: 0.3s;
+    color:red;
+  }
+          `}</style>
+        </Helmet>
     <div className="parent-of-model">
         <form className={`model`}  >
       
@@ -35,6 +85,7 @@ function Model( {closeFunc, children} ) {
           {/* {Resend == "Check Your Email Now" ? <p style={{color:"black"}}>{Resend}</p> : <p  style={{color:"black"}}> {Resend}</p>} */}
         </form>
       </div>
+    </>
   )
 }
 
